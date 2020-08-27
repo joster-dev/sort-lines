8bd5a148e9762e26c4b9d4a4d64dbc628fb57fe4
Joster422 <Joster422@gmail.com>
2020-08-27 19:28:46 -0400

    poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sort-lines';
+  model: string | null = null;
+  sortedModel = '';
+
+  constructor() { }
+
+  submit(): void {
+    if (typeof this.model !== 'string' || this.model.trim().length === 0) {
+      return;
+    }
+    this.sortedModel = this.model
+      .split('\n')
+      .sort((a, b) => {
+        const shortString = a.length < b.length
+          ? a
+          : b;
+        for (let i = 0; i < shortString.length; i++) {
+          const compare = a.charCodeAt(i) - b.charCodeAt(i);
+          if (compare < 0) {
+            return -1;
+          } else if (compare > 0) {
+            return 1;
+          }
+        }
+        return 1;
+      })
+      .join('\n');
+  }
 }
