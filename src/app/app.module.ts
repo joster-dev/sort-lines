e02715717ac395996332193920afb739c6cbf937
Joster422 <Joster422@gmail.com>
2020-08-27 20:06:13 -0400

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register(
+      'ngsw-worker.js',
+      { enabled: environment.production }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
